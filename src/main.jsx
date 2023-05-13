8cec3cd1b63153c15b387aead3eacb3edd16b7b6
Cristian Arias <javier.0813@hotmail.com>
2023-05-13 09:44:46 -0500

    Actualizacion

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,3 @@
-/* import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
- */
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -28,7 +16,7 @@ import SearchSeriesPeliculas from './components/searchseriespeliculas/SearchSeri
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/Inicio",
     element: <PeliculasTv />
   },
   {
